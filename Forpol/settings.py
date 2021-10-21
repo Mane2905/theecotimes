41f63c7293dc5d8ea052589fb76c7341d21d7d47
Mane2905 <abhayguptasmo05@gmail.com>
2021-10-21 19:14:02 +0530

    V1 Upload

--- Forpol/settings.py
+++ Forpol/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nr@84jhhb)n7m$!x$tex=bijwo4jl5fd(2s7_1$2f_6ygsu)ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
